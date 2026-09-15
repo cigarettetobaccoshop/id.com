971d480c9c4e9ede2dc2290af27de01fcb32eb6d
cigarettetobaccoshop <cigaratetobacoshop@gmail.com>
2026-09-16 05:09:51 +0700

    feat(home): mount courier partner slider runtime

--- components/monitoring/ActivityMonitoringRuntime.js
+++ components/monitoring/ActivityMonitoringRuntime.js
@@ -1,10 +1,11 @@
 import { useAuthMonitor } from '../../hooks/useAuthMonitor'
 import { useFormTracker } from '../../hooks/useFormTracker'
 import { useOnlineGuests } from '../../hooks/useOnlineGuests'
+import CourierPartners from '../homepage/CourierPartners'
 
 export default function ActivityMonitoringRuntime() {
   useAuthMonitor()
   useFormTracker()
   useOnlineGuests()
-  return null
+  return <CourierPartners />
 }
